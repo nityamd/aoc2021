25055e9902a1edbbbcf04d2a935a6692e001c847
nityamd <nityamd@gmail.com>
2021-12-03 21:59:15 -0500

    day 2 part 2

--- day2/solution.py
+++ day2/solution.py
@@ -1,8 +1,23 @@
+def traversal(manual):
+	horizontal, aim, depth = 0, 0, 0
+	for m in manual:
+		if m[0] == 'f':
+			horizontal += int(m[-1])
+			depth += aim*int(m[-1])
+		elif m[0] == 'd':
+			aim += int(m[-1])
+		else:
+			aim -= int(m[-1])
+	return horizontal*depth
+	
+
 if __name__ == "__main__":
 	with open('./input.txt') as f:
 		x = f.read()
 	forward = [int(y[0]) for y in x.split('forward ')[1:]]
 	down = [int(y[0]) for y in x.split('down ')[1:]]
 	up = [int(y[0]) for y in x.split('up ')[1:]]
+	
 
 	print("part 1", sum(forward)*(sum(down)-sum(up)))
+	print("part 2", traversal(x.split('\n')[:-1]))
